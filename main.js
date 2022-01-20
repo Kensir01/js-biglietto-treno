e1144a314ef084e9a9bb9f44c1c220835c0adffe
kensir96m <kensir96@gmail.com>
2022-01-20 17:35:01 +0100

    aggiunta console

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 // Distanza
 let distanza = parseInt(prompt('Inserisci i chilometri da percorrere'));
+console.log(distanza)
 
 // Eta
 let eta = parseInt(prompt('Digita eta del passeggiero'));
+console.log(eta)
 
 // Costo base del biglietto 
 let costo_biglietto = distanza * 0.21
+console.log(costo_biglietto)
 
 // Sconto minorenni
 let sconto_minorenni = costo_biglietto - ((costo_biglietto * 20) / 100)
+console.log(sconto_minorenni)
 
 // Sconto senior
 let sconto_senior = costo_biglietto - ((costo_biglietto * 40) / 100)
+console.log(sconto_senior)
 
 // Calcolo costo corsa
 if (eta < 18) {
