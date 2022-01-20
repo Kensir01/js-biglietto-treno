eeb3fb0126cdf3051be6020c3b7153a702f621a3
kensir96m <kensir96@gmail.com>
2022-01-20 17:33:00 +0100

    Aggiunto codice js

---
+++ main.js
@@ -0,0 +1,23 @@
+// Distanza
+let distanza = parseInt(prompt('Inserisci i chilometri da percorrere'));
+
+// Eta
+let eta = parseInt(prompt('Digita eta del passeggiero'));
+
+// Costo base del biglietto 
+let costo_biglietto = distanza * 0.21
+
+// Sconto minorenni
+let sconto_minorenni = costo_biglietto - ((costo_biglietto * 20) / 100)
+
+// Sconto senior
+let sconto_senior = costo_biglietto - ((costo_biglietto * 40) / 100)
+
+// Calcolo costo corsa
+if (eta < 18) {
+    document.getElementById('costo_corsa').innerHTML= 'Il prezzo del suo biglietto è: ' + sconto_minorenni.toFixed(2) + ' &euro; .';
+} else if (eta >= 65) {
+    document.getElementById('costo_corsa').innerHTML = 'Il costo del tuo biglietto è ' + sconto_senior.toFixed(2) + ' &euro; .';
+} else {
+    document.getElementById('costo_corsa').innerHTML = 'Il costo del tuo biglietto è ' + costo_biglietto.toFixed(2) + ' &euro; .';
+};
